c91a3dc796bdc17ce5bf49abb0d2fb62989b1bc7
M Ahsan Saifi <ahsansoftengineer@gmail.com>
2021-02-04 10:08:16 -0800

    Initial Commit

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from "@angular/core";
+
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  {
+    path: "/dashboard",
+    title: "Dashboard",
+    icon: "icon-chart-pie-36",
+    class: "active"
+  },
+  {
+    path: "/products",
+    title: "Products",
+    icon: "icon-molecule-40",
+    class: ""
+  },
+  {
+    path: "/product list",
+    title: "Product List",
+    icon: "icon-chart-pie-36",
+    class: ""
+  },
+  // {
+  //   path: "/maps",
+  //   title: "Maps",
+  //   icon: "icon-pin",
+  //   class: ""
+  // },
+  {
+    path: "/notifications",
+    title: "Notifications",
+    icon: "icon-bell-55",
+    class: ""
+  },
+
+  {
+    path: "/user",
+    title: "User Profile",
+    icon: "icon-single-02",
+    class: ""
+  },
+  {
+    path: "/tables",
+    title: "Table List",
+    icon: "icon-puzzle-10",
+    class: ""
+  },
+  {
+    path: "/typography",
+    title: "Typography",
+    icon: "icon-align-center",
+    class: ""
+  },
+  {
+    path: "/rtl",
+    title: "RTL Support",
+    icon: "icon-world",
+    class: ""
+  }
+];
+
+@Component({
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"]
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+    if (window.innerWidth > 991) {
+      return false;
+    }
+    return true;
+  }
+}
