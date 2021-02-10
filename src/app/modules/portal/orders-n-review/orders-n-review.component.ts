31384aed0acecf68055933c5e7f905628e3cc012
Ahsan <ahsansoftengineer@gmail.com>
2021-02-10 17:20:27 +0500

    Table Icons Working Correctly

--- src/app/modules/portal/orders-n-review/orders-n-review.component.ts
+++ src/app/modules/portal/orders-n-review/orders-n-review.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-import { ProductInterface } from "src/app/models/interfaces/product.interface";
-import { ProductService } from "src/app/services/product.service";
 import { OrderInterface } from "../../../models/interfaces/order.interface";
 import { OrderService } from "../../../services/order.service";
+
+// const StatusIcons: { [characterName: string]: string} =
+// {
+//   Cancel: 'icon-simple-remove',
+//   Pending: 'icon-button-pause',
+//   Urgent: 'icon-watch-time',
+//   OnWay: 'icon-user-run',
+//   OrderStatus: 'icon-sound-wave',
+// };
 
 @Component({
   selector: "app-orders-n-review",
@@ -12,8 +19,32 @@ import { OrderService } from "../../../services/order.service";
 export class OrdersNReviewComponent implements OnInit {
   public orders: OrderInterface[];
   constructor(private orderService: OrderService) {}
-
   ngOnInit(): void {
     this.orders = this.orderService.orders;
+  }
+  getClassOrderStatus(status: string) {
+    switch (status) {
+      case "0":
+        return "icon-simple-remove";
+        break;
+      case "1":
+        return "icon-button-pause";
+        break;
+      case "2":
+        return "icon-bell-55";
+        break;
+      case "3":
+        return "icon-user-run";
+        break;
+      case "5":
+        return "icon-sound-wave";
+        break;
+      case "4":
+        return "icon-check-2";
+        break;
+      default:
+        return "icon-check-2";
+        break;
+    }
   }
 }
